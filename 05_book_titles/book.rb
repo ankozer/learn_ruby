963a87f4d94ac8f764418a5e028e29e206ff6a5c
anton <ankozer@gmail.com>
2020-05-04 15:38:59 +0300

    add solution to exercise 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+  LITTLE_WORDS = %w(
+    and as but for if nor or so yet a an the at by in of off on per to up via over
+  )
+
+  attr_reader :title
+
+  def title=(title_str)
+    @title = titleize(title_str)
+  end
+
+  private
+
+  def titleize (text)
+    words = text.split
+
+    capitalized_words = words.map do |w|
+      next w if LITTLE_WORDS.include?(w)
+
+      w.capitalize
+    end
+
+    capitalized_words[0] = capitalized_words[0].capitalize
+    capitalized_words.join(' ')
+  end
 end
