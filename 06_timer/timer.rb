d376c0bb13538af2c4c99ccedd7d9d2937317aa5
anton <ankozer@gmail.com>
2020-05-04 15:41:43 +0300

    add solution to exercise 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
-  #write your code here
+  SECONDS_IN_HOUR=3600
+  SECONDS_IN_MINUTE=60
+
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / SECONDS_IN_HOUR
+    minutes = (@seconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    seconds = @seconds % SECONDS_IN_MINUTE
+    
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
+  def padded(time)
+    time < 10 ? "0" + time.to_s : time.to_s
+  end
 end
